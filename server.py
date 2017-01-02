c6afed65cd771f88b2028e21ecb10ad5082c6f65
Griffith Baker <gbaker@student.csdspartans.org>
2017-01-02 13:46:59 -0500

    Changed port so server will run on port 4000

--- server.py
+++ server.py
@@ -2,5 +2,4 @@ from geemusic import app
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
