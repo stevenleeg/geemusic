dbcf508e807cc539259a0713432da2b74d3d0669
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-01-26 15:35:37 -0600

    Add last.fm scrobble support

--- geemusic/controllers.py
+++ geemusic/controllers.py
@@ -6,6 +6,9 @@ from geemusic.utils.music import GMusicWrapper
 @app.route("/stream/<song_id>")
 def redirect_to_stream(song_id):
     stream_url = api.get_google_stream_url(song_id)
+    # Scrobble if Last.fm is setup
+    if environ['LAST_FM_ACTIVE']:
+        last_fm.execute(song_id)
 
     app.logger.debug('URL is %s' % stream_url)
 
--- geemusic/utils/last_fm.py
+++ geemusic/utils/last_fm.py
@@ -1,11 +1,72 @@
 # Last.fm Syncing Tool
+# Lean, fast, and functional
 import os
+import time
+import datetime
 import requests
+import md5
 
-def authorize():
-    # TODO: Create function
-    doNothing = True
+from gmusicapi import CallFailure, Mobileclient
 
-def scrobble(song_name, artist_name):
-    apiResp = requests.post('http://ws.audioscrobbler.com/2.0/', {'method': 'track.updateNowPlaying', 'apiKey': os.environ['LAST_FM_API'], 'track': song_name, 'artist': artist_name})
-    print apiResp.text
+api_head = 'http://ws.audioscrobbler.com/2.0/'
+secret = os.environ['LAST_FM_API_SECRET']
+
+def authorize(user_token):
+    params = {
+            'api_key': os.environ['LAST_FM_API'],
+            'method': 'auth.getSession',
+            'token': user_token
+            }
+    requestHash = hashRequest(params, secret)
+    params['api_sig'] = requestHash
+    apiResp = requests.post(api_head, params)
+    return apiResp.text
+
+def nowPlaying(song_name, artist_name, session_key):
+    params = {
+            'method': 'track.updateNowPlaying',
+            'api_key': os.environ['LAST_FM_API'],
+            'track': song_name,
+            'artist': artist_name,
+            'sk': session_key
+            }
+    requestHash = hashRequest(params, secret)
+    params['api_sig'] = requestHash
+    apiResp = requests.post(api_head, params)
+    return apiResp.text
+
+def scrobble(song_name, artist_name, session_key):
+    # Currently this sort of cheats the timestamp protocol
+    params = {
+            'method': 'track.scrobble',
+            'api_key': os.environ['LAST_FM_API'],
+            'timestamp': str( int( time.time() ),
+            'track': song_name,
+            'artist': artist_name,
+            'sk': session_key
+            }
+    requestHash = hashRequest(params, secret)
+    params['api_sig'] = requestHash
+    apiResp = requests.post(api_head, params)
+    return apiResp.text
+
+def hashRequest(obj, secretKey):
+    string = ''
+    items = obj.keys()
+    items.sort()
+    for i in items:
+        string += i
+        string += obj[i]
+    string += secretKey
+    stringToHash = string.encode('utf8')
+    requestHash = md5.new(stringToHash).hexdigest()
+    return requestHash
+
+def get_google_song(song_id):
+    info = Mobileclient.get_track_info(song_id)
+    return info
+
+def execute(song_id):
+    print "Finding: ", song_id
+    track = get_google_song(song_id)
+    scrobble(track.title, track.artist, os.environ['LAST_FM_SESSION_KEY'])
