d7842f5618aaf4ca0fd99e51f5d58b3ea0a211f9
TheModdersDen <TheModdersDen@hotmail.com>
2017-12-24 14:00:45 -0700

    Enviornment variable fix.

--- geemusic/intents/selection.py
+++ geemusic/intents/selection.py
@@ -187,8 +187,8 @@ def play_song(song_name, artist_name):
 @ask.intent("GeeMusicPlayArtistRadioIntent")
 def play_artist_radio(name):
 	if str(environ['USE_LIBRARY_FIRST']) is True:
-		return statement("Sorry, artist radios aren't currently supported at the moment.")
-	
+		# return statement("Sorry, artist radios aren't currently supported at the moment.")
+		return None
     # Fetch the artist
 	artist = api.get_artist(name)
 
