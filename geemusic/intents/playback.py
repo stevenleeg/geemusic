4442b829f608e0437365c02f5fe76b566aaf3789
Billy <william.d.ferg@gmail.com>
2018-02-05 04:36:33 -0800

    Fixed lang -> language.

--- geemusic/intents/playback.py
+++ geemusic/intents/playback.py
@@ -60,7 +60,7 @@ WORDS = {
 @ask.on_playback_stopped()
 def stopped(offset):
     queue.paused_offset = offset
-    app.logger.debug(WORDS[language]["stopped"] % offset)
+    app.logger.debug(WORDS[app.language]["stopped"] % offset)
 
 
 @ask.on_playback_started()
@@ -101,12 +101,12 @@ def start_over():
 
 @ask.intent('AMAZON.ResumeIntent')
 def resume():
-    return audio(WORDS[language]["resume"]).resume()
+    return audio(WORDS[app.language]["resume"]).resume()
 
 
 @ask.intent('AMAZON.PauseIntent')
 def pause():
-    return audio(WORDS[language]["pause"]).stop()
+    return audio(WORDS[app.language]["pause"]).stop()
 
 
 @ask.intent('AMAZON.StopIntent')
