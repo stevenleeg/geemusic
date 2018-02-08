0460953250f4392254d5934fd0d6a38742523a19
Billy Ferguson <william.d.ferg@gmail.com>
2018-02-08 16:46:40 +0000

    Remove AWS Lambda specific function.

--- geemusic/__init__.py
+++ geemusic/__init__.py
@@ -17,10 +17,6 @@ TEMPLATE_DIR = "templates/" + language + ".yaml"
 app = Flask(__name__)
 ask = Ask(app, '/alexa', path=TEMPLATE_DIR)
 
-def lambda_handler(event, _context):
-    return ask.run_aws_lambda(event)
-
-
 if str(environ['DEBUG_MODE']) is True:
     log_level = logging.DEBUG
 else:
