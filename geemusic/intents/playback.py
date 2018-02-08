e0c6c1d474c21da68ea85b42110863b28ac99b1e
Billy Ferguson <william.d.ferg@gmail.com>
2018-02-08 20:09:47 +0000

    Remove unused language.

--- geemusic/intents/playback.py
+++ geemusic/intents/playback.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_ask import statement, audio
-from geemusic import ask, queue, app, api, language
+from geemusic import ask, queue, app, api
 import json
 
 
