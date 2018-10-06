c6e4f3f1056ef747b181de4ab8f7ea86ff116591
Mike Miller <github@mikeage.net>
2018-10-06 18:39:12 +0000

    Fix error if a song has no thumbnail

--- geemusic/intents/playback.py
+++ geemusic/intents/playback.py
@@ -147,7 +147,10 @@ def currently_playing():
     if track is None:
         return audio(render_template("currently_playing_none"))
 
-    thumbnail = api.get_thumbnail(queue.current_track()['albumArtRef'][0]['url'])
+    if 'albumArtRef' in queue.current_track():
+        thumbnail = api.get_thumbnail(queue.current_track()['albumArtRef'][0]['url'])
+    else:
+        thumbnail = None
     return statement(render_template("success_title")\
                            + render_template("success_text",
                                              song=track['title'],
